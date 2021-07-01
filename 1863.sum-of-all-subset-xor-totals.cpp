9d080484dc646633311f71229225a590e27b0a55
niharika2k00 <dniharika16@gmail.com>
2021-07-02 00:24:41 +0530

    Xor

--- 1863.sum-of-all-subset-xor-totals.cpp
+++ 1863.sum-of-all-subset-xor-totals.cpp
@@ -10,6 +10,16 @@ class Solution
 public:
     int subsetXORSum(vector<int> &nums)
     {
+        // Obsesrve that after performing the sum we are actually performing the addition upto 2^n - 1.
+        int xor_sum = 0, i;
+        int limit = (2 * nums.size()) - 1;
+
+               for (i = 0; i <= limit; i++)
+        {
+            xor_sum = xor_sum + i;
+        }
+
+        return xor_sum;
     }
 };
 // @lc code=end
