1eeeddc6485ef5c4facbf405026de39e2a5c4b9b
niharika2k00 <dniharika16@gmail.com>
2021-07-01 23:53:28 +0530

    a

---
+++ 1863.sum-of-all-subset-xor-totals.cpp
@@ -0,0 +1,15 @@
+/*
+ * @lc app=leetcode id=1863 lang=cpp
+ *
+ * [1863] Sum of All Subset XOR Totals
+ */
+
+// @lc code=start
+class Solution {
+public:
+    int subsetXORSum(vector<int>& nums) {
+        
+    }
+};
+// @lc code=end
+
