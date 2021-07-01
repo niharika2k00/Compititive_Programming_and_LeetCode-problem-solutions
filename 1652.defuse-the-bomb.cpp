63e7d3cc98e321fe87a15ad15caf282ae1bc6ab0
niharika2k00 <dniharika16@gmail.com>
2021-07-01 23:47:55 +0530

    a

---
+++ 1652.defuse-the-bomb.cpp
@@ -0,0 +1,17 @@
+/*
+ * @lc app=leetcode id=1652 lang=cpp
+ *
+ * [1652] Defuse the Bomb
+ */
+
+// @lc code=start
+class Solution
+{
+public:
+    vector<int> decrypt(vector<int> &code, int k)
+    {
+
+        hdslk
+    }
+};
+// @lc code=end
