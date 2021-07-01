cecda4418965471bcde02cecfed7bbbd43b722cf
niharika2k00 <dniharika16@gmail.com>
2021-07-01 23:54:08 +0530

    a

--- 1863.sum-of-all-subset-xor-totals.cpp
+++ 1863.sum-of-all-subset-xor-totals.cpp
@@ -5,11 +5,14 @@
  */
 
 // @lc code=start
-class Solution {
+class Solution
+{
 public:
-    int subsetXORSum(vector<int>& nums) {
-        
+    int subsetXORSum(vector<int> &nums)
+    {
+
+    Explanation:
+        The 8 subsets of[5, 1, 6] are : -The empty subset has an XOR total of 0. - [5] has an XOR total of 5. - [1] has an XOR total of 1. - [6] has an XOR total of 6. - [ 5, 1 ] has an XOR total of 5 XOR 1 = 4. - [ 5, 6 ] has an XOR total of 5 XOR 6 = 3. - [ 1, 6 ] has an XOR total of 1 XOR 6 = 7. - [ 5, 1, 6 ] has an XOR total of 5 XO
     }
 };
 // @lc code=end
-
