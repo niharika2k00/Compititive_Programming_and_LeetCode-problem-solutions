568894fe98e77d55ae7a816bc473515fed81d895
niharika2k00 <dniharika16@gmail.com>
2021-07-01 23:49:13 +0530

    a

---
+++ 1752.check-if-array-is-sorted-and-rotated.cpp
@@ -0,0 +1,15 @@
+/*
+ * @lc app=leetcode id=1752 lang=cpp
+ *
+ * [1752] Check if Array Is Sorted and Rotated
+ */
+
+// @lc code=start
+class Solution {
+public:
+    bool check(vector<int>& nums) {
+        
+    }
+};
+// @lc code=end
+
