a99de64a0203bee594fc67285e44cc8ebcf58f49
niharika2k00 <dniharika16@gmail.com>
2021-07-02 00:30:29 +0530

    Xor

--- 1863.sum-of-all-subset-xor-totals.cpp
+++ 1863.sum-of-all-subset-xor-totals.cpp
@@ -12,9 +12,9 @@ public:
     {
         // Obsesrve that after performing the sum we are actually performing the addition upto 2^n - 1.
         int xor_sum = 0, i;
-        int limit = (2 * nums.size()) - 1;
+        int limit = pow(2, nums.size()) - 1;
 
-               for (i = 0; i <= limit; i++)
+        for (i = 0; i <= limit; i++)
         {
             xor_sum = xor_sum + i;
         }
