69b0189e1fe354c10a4b6504f62f6940097f85fc
niharika2k00 <dniharika16@gmail.com>
2021-07-05 01:02:44 +0530

    as

---
+++ 1624.largest-substring-between-two-equal-characters.cpp
@@ -0,0 +1,15 @@
+/*
+ * @lc app=leetcode id=1624 lang=cpp
+ *
+ * [1624] Largest Substring Between Two Equal Characters
+ */
+
+// @lc code=start
+class Solution {
+public:
+    int maxLengthBetweenEqualCharacters(string s) {
+        
+    }
+};
+// @lc code=end
+
