e03040afbea5bc19754d467c5a35a6a292e085e6
niharika2k00 <dniharika16@gmail.com>
2021-07-22 23:47:37 +0530

    asdfs

--- 2.add-two-numbers.cpp
+++ 2.add-two-numbers.cpp
@@ -15,39 +15,11 @@
  *     ListNode(int x, ListNode *next) : val(x), next(next) {}
  * };
  */
-class Solution
-{
+class Solution {
 public:
-    ListNode *addTwoNumbers(ListNode *l1, ListNode *l2)
-    {
-
-        int carry = 0, sum = 0;
-        ListNode *AddList = new ListNode();
-        ListNode *temp = AddList; // address of the start node of the new LinkedList
-
-        while (l1 != NULL || l2 != NULL || carry)
-        {
-
-            if (l1 != NULL)
-            {
-                sum = l1->val + sum;
-                l1 = l1->next;
-            }
-
-            if (l2 != NULL)
-            {
-                sum = sum + l2->val;
-                l2 = l2->next;
-            }
-
-            sum = sum + carry;
-            carry = sum / 10;
-            ListNode *node = new ListNode(sum % 10);
-            temp->next = node;
-            temp = temp->next;
-        }
-
-        return AddList->next;
+    ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
+        
     }
 };
 // @lc code=end
+
