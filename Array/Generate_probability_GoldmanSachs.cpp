ff463a69b89f6f271026f83348d65d0e1898a277
Niharika Dutta <dniharika16@gmail.com>
2021-08-20 01:56:37 +0530

    sa

---
+++ Array/Generate_probability_GoldmanSachs.cpp
@@ -0,0 +1,58 @@
+/* 
+________________________________________
+----------------------------------------
+ Author :  Niharika Dutta
+ Code Link :   https://www.geeksforgeeks.org/generate-0-1-25-75-probability/ 
+________________________________________
+----------------------------------------
+ */
+#include <bits/stdc++.h>
+#include <iostream>
+
+using namespace std;
+
+#define ll long long
+#define vi vector<int>
+#define vll vector<ll>
+#define pb push_back
+#define mp make_pair
+#define loop1(n) for (ll i = 0; i < (n); i++)
+#define loop2(n) for (ll i = 1; i <= (n); i++)
+#define test  \
+    ll t;     \
+    cin >> t; \
+    while (t--)
+
+// Random Function to that returns 0 or 1 with equal probability
+int rand50()
+{
+    // rand() function will generate odd or even
+    // number with equal probability. If rand()
+    // generates odd number, the function will
+    // return 1 else it will return 0.
+    return rand() & 1;
+}
+
+// Returns 1 with 75% probability and 0 with 25% probability using Bitwise OR
+bool rand75()
+{
+    return rand50() | rand50();
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    srand(time(NULL));
+    unsigned char a = 6, b = 11;
+
+    // The result is 00000001
+    printf("a = %d, b = %d\n", a, b);
+
+    for (int i = 0; i < 50; i++)
+        cout << rand75();
+
+    return 0;
+}
