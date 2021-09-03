ef447493a2bb6c9de788a712c74cf248039e32e4
niharika2k00 <dniharika16@gmail.com>
2021-09-04 03:24:10 +0530

    aa

--- Binary_Search/Searching_In_Nearly_Sorted_Array.cpp
+++ Binary_Search/Searching_In_Nearly_Sorted_Array.cpp
@@ -24,6 +24,14 @@ using namespace std;
     cin >> t; \
     while (t--)
 
+/* 
+Nearly Sorted Array :: 
+
+- Nor Fully sorted .
+- the ith element can be in (i + 1 )th position , i th position or (i - 1)th position.
+
+ */
+
 int searchNearlySortedArray(int arr[], int start, int end, int findNum)
 {
     int mid = (start + end) / 2;
@@ -32,12 +40,19 @@ int searchNearlySortedArray(int arr[], int start, int end, int findNum)
         if (findNum == arr[mid])
             return mid;
 
-        if (start <= mid - 1 && arr[mid - 1] == findNum)
+        else if (start <= mid - 1 && arr[mid - 1] == findNum)
             return mid - 1;
 
-        if (mid + 1 <= end && arr[mid + 1] == findNum)
+        else if (mid + 1 <= end && arr[mid + 1] == findNum)
             return mid + 1;
+
+        else if (findNum < arr[mid])
+            return searchNearlySortedArray(arr, start, mid - 2, findNum);
+
+        else if (findNum > arr[mid])
+            return searchNearlySortedArray(arr, mid + 2, end, findNum);
     }
+    return -1;
 }
 
 int main()
@@ -46,8 +61,8 @@ int main()
     cin.tie(NULL);
     cout.tie(NULL);
 
-    int arr[] = {42, 5, 8, 10, 16}, findNum = 10, res = 0, size = sizeof(arr) / sizeof(arr[0]);
+    int arr[] = {42, 55, 6, 10, 16}, findNum = 42, res = 0, size = sizeof(arr) / sizeof(arr[0]);
     res = searchNearlySortedArray(arr, 0, size - 1, findNum);
-
+    cout << res << endl;
     return 0;
 }
