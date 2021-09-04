8da3e3b4000e2796d0592558d12d362b31cc62a4
niharika2k00 <dniharika16@gmail.com>
2021-09-05 00:45:36 +0530

    atcoder

--- Binary_Search/Floor_and_Ceil_of_Element.cpp
+++ Binary_Search/Floor_and_Ceil_of_Element.cpp
@@ -25,7 +25,6 @@ using namespace std;
     while (t--)
 
 /* 
-
   # FLOOR & CEIL  ELEMENT ::
          when an element is just less than the given number means arr = { 2 3 5 8 } , key = 4 ............ then  ans = 3 (floor) ans ans = 5 (Ceil)
   
@@ -35,78 +34,59 @@ using namespace std;
 int find_Floor(int arr[], int start, int end, int key)
 {
     int mid, store = INT_MAX;
-
-    // if (start <= end)
-    while (end > start)
+    while (end >= start)
     {
         mid = (start + end) / 2;
         if (arr[mid] == key)
-            return mid;
+            return arr[mid];
+
         else if (key < arr[mid]) //  LEFT
             end = mid - 1;
-        // return find_Floor(arr, start, mid - 1, key);
 
-        // else if (arr[mid - 1] <= key && key < arr[mid])
-        // return (mid - 1);
-
-        else if (key > arr[mid])
+        else if (key > arr[mid]) // RIGHT --- means when the key is large than the mid then Store the MID in a variable for finding the nearest FLOOR Value.
         {
             // if (store > arr[mid])
             store = arr[mid];
             start = mid + 1;
-            // return find_Floor(arr, mid + 1, end, key);
         }
     }
+    return store;
+}
 
+// Ceil Value finder (Lower than the key)
+int find_Ceil(int arr[], int start, int end, int key)
+{
+    int mid, store = INT_MIN;
+    while (end >= start)
     {
-        // If start and high cross each other
-        //if (low > high)
-        //	return -1;
+        mid = (start + end) / 2;
+        if (arr[mid] == key)
+            return arr[mid];
 
-        if (end >= start)
+        else if (key < arr[mid]) //  LEFT
         {
-            // If last element is smaller than x
-            if (key >= arr[end])
-                return end;
-
-            mid = (start + end) / 2;
-
-            if (arr[mid] == key)
-                return mid;
-
-            // If key lies between mid-1 and mid
-            if (mid > 0 && arr[mid - 1] <= key && key < arr[mid])
-                return mid - 1;
-
-            if (key < arr[mid]) // LEFT
-                return find_Floor(arr, start, mid - 1, key);
-
-            // If mid-1 is not floor and key is greater than arr[mid],
-            // key is more than arr[mid] means arr[mid] is one of the floor(LESS) value in comparison to key  RIGHT
-            else
-                return find_Floor(arr, mid + 1, end, key);
+            store = arr[mid];
+            end = mid - 1;
         }
-        return -1;
+
+        else if (key > arr[mid]) // RIGHT --- means when the key is large than the mid then Store the MID in a variable for finding the nearest FLOOR Value.
+            start = mid + 1;
     }
+    return store;
+}
 
-    int find_Ceil(int arr[], int start, int end, int key)
-    {
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
 
-        return 0;
-    }
+    int arr[] = {2, 4, 10, 16, 22, 30, 88}, key = 28, floorResult = 0, ceilResult = 0, size = sizeof(arr) / sizeof(arr[0]);
 
-    int main()
-    {
-        ios_base::sync_with_stdio(false);
-        cin.tie(NULL);
-        cout.tie(NULL);
+    floorResult = find_Floor(arr, 0, size - 1, key);
+    ceilResult = find_Ceil(arr, 0, size - 1, key);
 
-        int arr[] = {2, 4, 10, 16, 22, 30, 88}, key = 22, floorResult = 0, ceilResult = 0, size = sizeof(arr) / sizeof(arr[0]);
+    cout << "Floor Value  : " << floorResult << "\tCeil Value  : " << ceilResult << endl;
 
-        floorResult = find_Floor(arr, 0, size - 1, key);
-        ceilResult = find_Ceil(arr, 0, size - 1, key);
-
-        cout << "asn : " << floorResult << endl;
-
-        return 0;
-    }
+    return 0;
+}
