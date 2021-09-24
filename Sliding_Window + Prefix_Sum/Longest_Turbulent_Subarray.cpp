d6d3eb417c244ff92d1ac667a33420f6b0dab7dd
niharika2k00 <dniharika16@gmail.com>
2021-09-24 13:32:41 +0530

    3322

--- Sliding_Window + Prefix_Sum/Longest_Turbulent_Subarray.cpp
+++ Sliding_Window + Prefix_Sum/Longest_Turbulent_Subarray.cpp
@@ -55,4 +55,76 @@ int main()
     cout << maxTrendLength << endl;
 
     return 0;
+
+    if (arr.size() == 0)
+        return 0;
+    if (arr.size() == 1)
+        return 1;
+    bool flag = true;
+
+    for (int i = 0; i < arr.size() - 1; i++)
+    {
+        if (arr[i] != arr[i + 1])
+            flag = false;
+    }
+
+    if (flag)
+        return 1;
+    int maxlen = 2;
+    int l = 0, r = 0;
+    int nextSmall = false;
+
+    if (arr[0] > arr[1])
+        nextSmall = true;
+
+    for (int i = 1; i < arr.size() - 1; i++)
+    {
+        if (arr[i] == arr[i + 1])
+        {
+            l = r = i;
+            nextSmall = -1;
+        }
+        if (nextSmall == -1)
+        {
+            if (arr[i] < arr[i + 1])
+            {
+                l = r = i;
+                nextSmall = false;
+            }
+            else if (arr[i] > arr[i + 1])
+            {
+                l = r = i;
+                nextSmall = true;
+            }
+        }
+        else if (nextSmall == 1)
+        {
+            if (arr[i] < arr[i + 1])
+            {
+                r = i + 1;
+                maxlen = max(maxlen, r - l + 1);
+                nextSmall = !nextSmall;
+            }
+            else
+            {
+                l = r = i;
+                nextSmall = true;
+            }
+        }
+        else
+        {
+            if (arr[i] > arr[i + 1])
+            {
+                r = i + 1;
+                maxlen = max(maxlen, r - l + 1);
+                nextSmall = !nextSmall;
+            }
+            else
+            {
+                l = r = i;
+                nextSmall = false;
+            }
+        }
+    }
+    return maxlen;
 }
