08aae11062ba92ef6649b1a31bb279c950320027
niharika2k00 <dniharika16@gmail.com>
2021-09-26 03:10:54 +0530

    tree

---
+++ Leet_Code_Problems/226.invert-binary-tree.cpp
@@ -0,0 +1,66 @@
+/*
+ * @lc app=leetcode id=226 lang=cpp
+ *
+ * [226] Invert Binary Tree
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+// class Solution
+// {
+// public:
+//     // USING RECURSION METHOD INVERTING THE EXSISTING TREE
+//     TreeNode *invertTree(TreeNode *root)
+//     {
+
+//         if (!root)
+//             return NULL;
+
+//         swap(root->right, root->left);
+//         invertTree(root->left);
+//         invertTree(root->right);
+
+//         return root;
+//     }
+// };
+// @lc code=end
+
+class Solution
+{
+public:
+    // USING STACK DATA STRUCTURE METHOD INVERTING THE EXSISTING TREE
+    TreeNode *invertTree(TreeNode *root)
+    {
+        if (!root)
+            return NULL;
+
+        stack<*TreeNode> stack;
+        stack.push(root);
+
+        while (stack.size() > 0)
+        {
+            TreeNode *node = stack.top();
+            stack.pop();
+
+            swap(node->right, node->left);
+
+            if (root->left)
+                stack.push(root->left);
+
+            if (root->right)
+                stack.push(root->right);
+        }
+
+        return root;
+    }
+};
