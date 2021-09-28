7c48f22267b17c7c50da46c61ca6110b6ab19eb9
niharika2k00 <dniharika16@gmail.com>
2021-09-28 23:53:48 +0530

    ss

---
+++ CodeForces/A_Casimir_s_String_Solitaire.cpp
@@ -0,0 +1,58 @@
+/* 
+________________________________________
+----------------------------------------
+ Author    :  Niharika Dutta
+ Code Link : https://codeforces.com/contest/1579/problem/A   
+ Time Complexity :  
+________________________________________
+----------------------------------------
+ */
+#include <bits/stdc++.h>
+#include <iostream>
+
+using namespace std;
+
+#define ll long long
+#define lli long long int
+#define vi vector<int>
+#define vll vector<ll>
+#define pb push_back
+#define mp make_pair
+#define loop1(n) for (ll i = 0; i < (n); i++)
+#define loop2(n) for (ll i = 1; i <= (n); i++)
+#define test  \
+    ll t;     \
+    cin >> t; \
+    while (t--)
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    test
+    {
+        int a = 0, b = 0, c = 0, i = 0;
+        string s;
+        cin >> s;
+
+        for (i = 0; i < s.size(); i++)
+        {
+            if (s[i] == 'A')
+                a++;
+            else if (s[i] == 'B')
+                b++;
+            else
+                c++;
+        }
+
+        int ans = a + c;
+        if (ans == b)
+            cout << "YES" << endl;
+        else
+            cout << "NO" << endl;
+    }
+
+    return 0;
+}
