d0c6f6003295626f292ba7dc3b53defde92e67ba
niharika2k00 <dniharika16@gmail.com>
2021-10-04 13:29:59 +0530

    2 pointer

---
+++ Leet_Code_Problems/1431.kids-with-the-greatest-number-of-candies.cpp
@@ -0,0 +1,15 @@
+/*
+ * @lc app=leetcode id=1431 lang=cpp
+ *
+ * [1431] Kids With the Greatest Number of Candies
+ */
+
+// @lc code=start
+class Solution {
+public:
+    vector<bool> kidsWithCandies(vector<int>& candies, int extraCandies) {
+        
+    }
+};
+// @lc code=end
+
