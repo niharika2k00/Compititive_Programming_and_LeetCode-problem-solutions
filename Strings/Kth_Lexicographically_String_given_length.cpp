f712114ddfcc6f0a3d3070e0e73f1c4b3a50d321
niharika2k00 <dniharika16@gmail.com>
2021-10-19 22:20:11 +0530

    lexi

--- Strings/Kth_Lexicographically_String_given_length.cpp
+++ Strings/Kth_Lexicographically_String_given_length.cpp
@@ -6,7 +6,7 @@ ________________________________________
  Time Complexity :  
 ________________________________________
 ----------------------------------------
- */ 
+ */
 #include <bits/stdc++.h>
 #include <iostream>
 
@@ -20,15 +20,49 @@ using namespace std;
 #define mp make_pair
 #define loop1(n) for (ll i = 0; i < (n); i++)
 #define loop2(n) for (ll i = 1; i <= (n); i++)
-#define test   ll t;  cin >> t;   while (t--)
+#define test  \
+    ll t;     \
+    cin >> t; \
+    while (t--)
 
- 
+string lexicographicalString(int len, int k)
+{
+    int i, temp[len] = {0};
+    string newString = "";
+
+    for (i = len - 1; i >= 0; i--)
+    {
+        temp[i] = k % 26;
+        k /= 26;
+        cout << "k = " << k << endl;
+    }
+
+    if (k > 0)
+        return "-1";
+
+    for (i = 0; i < len; i++)
+        newString += (temp[i] + ('a'));
+
+    return newString;
+}
+
 int main()
 {
-ios_base::sync_with_stdio(false);
-cin.tie(NULL);
- cout.tie(NULL);
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
 
-    
- return 0;
+    int len = 3, k = 10; // K-th lexicographical string of length len
+    // if (pow(26, len) < k)
+    //     cout << "-1\n";
+    k = k - 1; // bcz stored value start from 0
+    cout << lexicographicalString(len, k);
+
+    return 0;
 }
+
+/* 
+         1) First fill all the indexes wuth 0
+         2) then from the back increases lexicographically
+          aaaa -> aaab -> aaac            ... like this
+ */
