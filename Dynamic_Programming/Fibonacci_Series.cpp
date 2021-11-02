96fafbd44c95e1a3cff9803c49bafcdd98ed8405
niharika2k00 <dniharika16@gmail.com>
2021-11-02 23:19:15 +0530

    leetcode_DP

--- Dynamic_Programming/Fibonacci_Series.cpp
+++ Dynamic_Programming/Fibonacci_Series.cpp
@@ -25,11 +25,26 @@ using namespace std;
     cin >> t; \
     while (t--)
 
+int fibonacci(int n)
+{
+    if (n == 0)
+        return 0;
+    if (n == 1)
+        return 1;
+    else
+        return (fibonacci(n - 1) + fibonacci(n - 2));
+}
+
 int main()
 {
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
     cout.tie(NULL);
+
+    int n;
+    cin >> n;
+
+    cout << fibonacci(n);
 
     return 0;
 }
