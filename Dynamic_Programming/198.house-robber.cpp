88e8d8a9f0fb2d6c6ce2ad099e08aee478cf5990
niharika2k00 <dniharika16@gmail.com>
2021-11-03 14:07:04 +0530

    sd

--- Dynamic_Programming/198.house-robber.cpp
+++ Dynamic_Programming/198.house-robber.cpp
@@ -10,37 +10,40 @@ class Solution
 public:
     int rob(vector<int> &nums)
     {
-        int i, len = nums.size(), sum1 = 0, sum2 = 0;
+        // USING IF - ELSE
+        // int i, len = nums.size(), sum1 = 0, sum2 = 0;
 
-        for (i = 0; i < len; i++)
-        {
-            if (i % 2 == 0) // even
-            {
-                sum1 = sum1 + nums[i];
-                sum1 = max(sum1, sum2);
-            }
-            else
-            {
-                sum2 = sum2 + nums[i];
-                sum2 = max(sum2, sum1);
-            }
-        }
+        // for (i = 0; i < len; i++)
+        // {
+        //     if (i % 2 == 0) // even
+        //     {
+        //         sum1 = sum1 + nums[i];
+        //         sum1 = max(sum1, sum2);
+        //     }
+        //     else
+        //     {
+        //         sum2 = sum2 + nums[i];
+        //         sum2 = max(sum2, sum1);
+        //     }
+        // }
 
-        return max(sum1, sum2);
+        // return max(sum1, sum2);
 
-        // int len = nums.size();
-        // vector<int> dp[len];
+        // USING LOOP
+        int len = nums.size(), i;
+        vector<int> dp(len, 0);
 
-        // if (nums.empty())
-        //     return 0;
-        // if (len == 1)
-        //     return nums[0];
+        if (nums.empty() || len == 0)
+            return 0;
+        if (len == 1)
+            return nums[0];
 
-        // dp[0] = nums[0], dp[1] = max(nums[0], nums[1]);
+        dp[0] = nums[0];
+        dp[1] = max(nums[0], nums[1]);
 
-        // for (int i = 2; i < len; i++)
-        //     dp[i] = max(dp[i - 1], nums[i] + dp[i - 2]);
-        // return dp[len - 1];
+        for (i = 2; i < len; ++i)
+            dp[i] = max(dp[i - 1], nums[i] + dp[i - 2]);
+        return dp[len - 1];
     }
 };
 // @lc code=end
