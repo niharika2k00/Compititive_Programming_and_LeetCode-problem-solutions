477312f77a5b77a42e37ddfcdcb3d92cc3eda1d5
niharika2k00 <dniharika16@gmail.com>
2021-11-03 22:36:45 +0530

    sds

---
+++ Leet_Code_Problems/55.jump-game.cpp
@@ -0,0 +1,20 @@
+/*
+ * @lc app=leetcode id=55 lang=cpp
+ *
+ * [55] Jump Game
+ */
+
+// @lc code=start
+class Solution
+{
+public:
+    bool canJump(vector<int> &nums)
+    {
+
+        int len = nums.size();
+
+        if (nums[0] == 0)
+            return false;
+    }
+};
+// @lc code=end
