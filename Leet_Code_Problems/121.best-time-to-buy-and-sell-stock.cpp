3e742074ec47026a561c429acbae7b01356a4965
niharika2k00 <dniharika16@gmail.com>
2021-11-09 17:40:37 +0530

    asds

---
+++ Leet_Code_Problems/121.best-time-to-buy-and-sell-stock.cpp
@@ -0,0 +1,15 @@
+/*
+ * @lc app=leetcode id=121 lang=cpp
+ *
+ * [121] Best Time to Buy and Sell Stock
+ */
+
+// @lc code=start
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        
+    }
+};
+// @lc code=end
+
---
+++ Leet_Code_Problems/123.best-time-to-buy-and-sell-stock-iii.cpp
@@ -0,0 +1,15 @@
+/*
+ * @lc app=leetcode id=123 lang=cpp
+ *
+ * [123] Best Time to Buy and Sell Stock III
+ */
+
+// @lc code=start
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        
+    }
+};
+// @lc code=end
+
