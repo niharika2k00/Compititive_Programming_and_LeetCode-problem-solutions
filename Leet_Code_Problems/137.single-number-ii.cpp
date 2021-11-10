fbdd0d4f9cffa1128b172e208960e62817a3fed4
niharika2k00 <dniharika16@gmail.com>
2021-11-10 13:20:51 +0530

    single number

--- Leet_Code_Problems/137.single-number-ii.cpp
+++ Leet_Code_Problems/137.single-number-ii.cpp
@@ -5,11 +5,29 @@
  */
 
 // @lc code=start
-class Solution {
+class Solution
+{
 public:
-    int singleNumber(vector<int>& nums) {
-        
+    int singleNumber(vector<int> &nums)
+    {
+        // int i, ans = 0;
+        // for (i = 0; i < nums.size(); i++)
+        //     ans = ans ^ nums[i]; //  Xor of 2 ^ 2 = 0
+        // return ans;
+
+        unordered_map<int, int> map;
+        int i, ans = 0;
+
+        for (i = 0; i < nums.size(); i++)
+            map[nums[i]]++;
+
+        for (auto j = map.begin(); j != map.end(); j++)
+        {
+            if (j->second == 1)
+                ans = j->first;
+        }
+
+        return ans;
     }
 };
 // @lc code=end
-
