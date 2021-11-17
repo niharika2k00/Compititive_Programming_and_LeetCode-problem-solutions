69a418789cca391381ec5393c4148165391646fc
niharika2k00 <dniharika16@gmail.com>
2021-11-17 18:33:42 +0530

    array

---
+++ Array/Find_Pair_With_Given_Sum.cpp
@@ -0,0 +1,49 @@
+/*
+________________________________________
+----------------------------------------
+ Author    :  Niharika Dutta
+ Code Link :
+ Time Complexity :
+________________________________________
+----------------------------------------
+ */
+#include <bits/stdc++.h>
+#include <iostream>
+
+using namespace std;
+
+#define ll long long
+#define lli long long int
+#define vi vector<int>
+#define vll vector<ll>
+#define pb push_back
+#define mp make_pair
+#define loop1(n) for (ll i = 0; i < (n); i++)
+#define loop2(n) for (ll i = 1; i <= (n); i++)
+#define test  \
+    ll t;     \
+    cin >> t; \
+    while (t--)
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    return 0;
+}
+
+/* int i, rem = 0;
+for (i = 0; i < n; i++)
+{
+
+    if (arr[i] <= x)
+    {
+        rem = x - arr[i];
+        int term = *find(arr, arr + n, rem);
+        if (term != 0) //  !0  -->  true
+            return 1;
+    }
+}
+return 0; */
