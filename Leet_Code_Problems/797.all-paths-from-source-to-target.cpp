6625a5eef27343eea880ef6a3c7f581f5a08d415
niharika2k00 <dniharika16@gmail.com>
2021-11-30 23:58:29 +0530

    leetcode

---
+++ Leet_Code_Problems/797.all-paths-from-source-to-target.cpp
@@ -0,0 +1,15 @@
+/*
+ * @lc app=leetcode id=797 lang=cpp
+ *
+ * [797] All Paths From Source to Target
+ */
+
+// @lc code=start
+class Solution {
+public:
+    vector<vector<int>> allPathsSourceTarget(vector<vector<int>>& graph) {
+        asdf
+    }
+};
+// @lc code=end
+
