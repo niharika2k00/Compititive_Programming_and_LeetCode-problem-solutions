f72051675ffc06bb583eb2aa7389867d57d8f67e
niharika2k00 <dniharika16@gmail.com>
2021-12-07 23:49:02 +0530

    Leet code

---
+++ Leet_Code_Problems/1217.minimum-cost-to-move-chips-to-the-same-position.cpp
@@ -0,0 +1,16 @@
+/*
+ * @lc app=leetcode id=1217 lang=cpp
+ *
+ * [1217] Minimum Cost to Move Chips to The Same Position
+ */
+
+// @lc code=start
+class Solution
+{
+public:
+    int minCostToMoveChips(vector<int> &position)
+    {
+        xdcd
+    }
+};
+// @lc code=end
---
+++ Leet_Code_Problems/1466.reorder-routes-to-make-all-paths-lead-to-the-city-zero.cpp
@@ -0,0 +1,15 @@
+/*
+ * @lc app=leetcode id=1466 lang=cpp
+ *
+ * [1466] Reorder Routes to Make All Paths Lead to the City Zero
+ */
+
+// @lc code=start
+class Solution {
+public:
+    int minReorder(int n, vector<vector<int>>& connections) {
+        
+    }
+};
+// @lc code=end
+
