a23c6a6569d81a73cc6ed16de8e8c93f087ebca5
niharika2k00 <dniharika16@gmail.com>
2021-12-16 18:44:48 +0530

    leetcode

---
+++ Leet_Code_Problems/1306.jump-game-iii.cpp
@@ -0,0 +1,15 @@
+/*
+ * @lc app=leetcode id=1306 lang=cpp
+ *
+ * [1306] Jump Game III
+ */
+
+// @lc code=start
+class Solution {
+public:
+    bool canReach(vector<int>& arr, int start) {
+        
+    }
+};
+// @lc code=end
+
