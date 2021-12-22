9b23a03357d1faa28f25ec5031691e56c2bcaf66
niharika2k00 <dniharika16@gmail.com>
2021-12-22 15:42:37 +0530

    problems

---
+++ Leet_Code_Problems/110.balanced-binary-tree.cpp
@@ -0,0 +1,29 @@
+/*
+ * @lc app=leetcode id=110 lang=cpp
+ *
+ * [110] Balanced Binary Tree
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ *
+ * https://leetcode.com/problems/balanced-binary-tree/
+ *
+ */
+class Solution
+{
+public:
+    bool isBalanced(TreeNode *root)
+    {
+    }
+};
+// @lc code=end
