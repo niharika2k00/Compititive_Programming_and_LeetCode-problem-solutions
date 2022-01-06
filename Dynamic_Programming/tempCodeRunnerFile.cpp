ac6a2d9b5cad030777f09eb81a628ae8b3ed0a57
niharika2k00 <dniharika16@gmail.com>
2022-01-06 22:07:28 +0530

    dp knapsack

--- Dynamic_Programming/tempCodeRunnerFile.cpp
+++ Dynamic_Programming/tempCodeRunnerFile.cpp
@@ -1,15 +1,59 @@
-int consider, notConsider;
-// if (len == 0 && sum == 0)
-//     return 1;
 
-// if (len == 0 && sum != 0)
-//     return 0;
+/*
+______________________________________
+----------------------------------------
+ Author    :  Niharika Dutta
+ Code Link :    https://www.geeksforgeeks.org/0-1-knapsack-problem-dp-10/
+ Time Complexity :
+________________________________________
+----------------------------------------
+ */
+#include <bits/stdc++.h>
+#include <iostream>
 
-// // When arr Element is Greater than SUM .
-// if (arr[len - 1] > sum)
-//     return Subset_CountFinder(arr, sum, len - 1); //  Not Consider
+using namespace std;
 
-// consider = Subset_CountFinder(arr, sum - arr[len - 1], len - 1); //  Consider
-// notConsider = Subset_CountFinder(arr, sum, len - 1);             // Not Consider
+#define ll long long
+#define lli long long int
+#define vi vector<int>
+#define vll vector<ll>
+#define pb push_back
+#define mp make_pair
+#define loop1(n) for (ll i = 0; i < (n); i++)
+#define loop2(n) for (ll i = 1; i <= (n); i++)
+#define test  \
+    ll t;     \
+    cin >> t; \
+    while (t--)
 
-// return consider + notConsider;
+int knapSack(int Capacity, int weight[], int profit[], int len)
+{
+    int consider = 0, notConsider = 0;
+    if (len == 0 || Capacity == 0)
+        return 0;
+
+    if (weight[len - 1] > Capacity)
+        return knapSack(Capacity, weight, profit, len - 1);
+
+    else
+    {
+        consider = profit[len - 1] + knapSack(Capacity - weight[len - 1], weight, profit, len - 1);
+        notConsider = knapSack(Capacity, weight, profit, len - 1);
+
+        return max(consider, notConsider);
+    }
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    int profit[] = {60, 100, 120};
+    int wt[] = {10, 20, 30};
+    int Capacity = 35, len = sizeof(profit) / sizeof(profit[0]);
+    cout << knapSack(Capacity, wt, profit, len);
+
+    return 0;
+}
