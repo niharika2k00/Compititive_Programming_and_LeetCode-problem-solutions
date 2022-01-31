4f6084dce012da3ba0f2d7b7cee352fd0581a2e0
niharika2k00 <dniharika16@gmail.com>
2022-02-01 03:10:52 +0530

    cc

---
+++ Leet_Code_Problems/1672.richest-customer-wealth.cpp
@@ -0,0 +1,15 @@
+/*
+ * @lc app=leetcode id=1672 lang=cpp
+ *
+ * [1672] Richest Customer Wealth
+ */
+
+// @lc code=start
+class Solution {
+public:
+    int maximumWealth(vector<vector<int>>& accounts) {
+        
+    }
+};
+// @lc code=end
+
