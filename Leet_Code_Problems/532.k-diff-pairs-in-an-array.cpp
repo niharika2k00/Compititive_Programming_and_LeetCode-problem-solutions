2ff7c212421e3d8ba2b5e098de919ebe697c0aad
niharika2k00 <dniharika16@gmail.com>
2022-02-09 21:53:53 +0530

    hashmap good prob

---
+++ Leet_Code_Problems/532.k-diff-pairs-in-an-array.cpp
@@ -0,0 +1,42 @@
+/*
+ * @lc app=leetcode id=532 lang=cpp
+ *
+ * https://leetcode.com/problems/k-diff-pairs-in-an-array/
+ *
+ * [532] K-diff Pairs in an Array
+ */
+
+// @lc code=start
+class Solution
+{
+public:
+    int findPairs(vector<int> &nums, int k)
+    {
+        int i, ans = 0, len = nums.size();
+        unordered_map<int, int> hashMap;
+
+        if (k < 0)
+            return 0;
+
+        //  Initialization
+        for (i = 0; i < len; i++)
+            hashMap[nums[i]]++;
+
+        //  Pair Finding
+        for (auto it : hashMap)
+        {
+            if ((k > 0 && hashMap.find(it.first + k) != hashMap.end()) || k == 0 && hashMap[it.first + k] > 1)
+                ans++;
+        }
+
+        return ans;
+    }
+};
+
+/*
+     If  K > 0 ,
+                 check whether (ele + K) is present in the MAP.
+     Else If  K == 0 ,
+                  check whether (ele + K) exsist  > 1
+ */
+// @lc code=end
