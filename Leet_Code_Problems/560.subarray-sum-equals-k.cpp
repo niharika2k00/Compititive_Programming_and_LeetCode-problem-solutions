80df177d9306bff8137860878975fbd16f2c21fc
niharika2k00 <dniharika16@gmail.com>
2022-02-12 23:40:37 +0530

    dth

---
+++ Leet_Code_Problems/560.subarray-sum-equals-k.cpp
@@ -0,0 +1,18 @@
+/*
+ * @lc app=leetcode id=560 lang=cpp
+ *
+ * https://leetcode.com/problems/subarray-sum-equals-k/
+ *
+ * [560] Subarray Sum Equals K
+ */
+
+// @lc code=start
+class Solution
+{
+public:
+    int subarraySum(vector<int> &nums, int k)
+    {
+        int len = nums.size(), i;
+        }
+};
+// @lc code=end
