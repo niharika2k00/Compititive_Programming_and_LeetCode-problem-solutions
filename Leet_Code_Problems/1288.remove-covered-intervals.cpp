12ae3d9ba89e94e58cf3309fbcf899afb93fa7ee
niharika2k00 <dniharika16@gmail.com>
2022-02-21 23:37:43 +0530

    pq

---
+++ Leet_Code_Problems/1288.remove-covered-intervals.cpp
@@ -0,0 +1,15 @@
+/*
+ * @lc app=leetcode id=1288 lang=cpp
+ *
+ * [1288] Remove Covered Intervals
+ */
+
+// @lc code=start
+class Solution {
+public:
+    int removeCoveredIntervals(vector<vector<int>>& intervals) {
+        
+    }
+};
+// @lc code=end
+
