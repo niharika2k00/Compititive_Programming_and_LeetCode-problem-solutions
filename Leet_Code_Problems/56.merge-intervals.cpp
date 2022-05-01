4ccf35ffab02dfb1b798cd41ba478f3b1d382c54
niharika2k00 <dniharika16@gmail.com>
2022-05-02 02:51:06 +0530

    SDE day 2

--- Leet_Code_Problems/56.merge-intervals.cpp
+++ Leet_Code_Problems/56.merge-intervals.cpp
@@ -1,15 +1,46 @@
 /*
  * @lc app=leetcode id=56 lang=cpp
  *
+ * https://leetcode.com/problems/merge-intervals/
+ * Similar Problem :  https://leetcode.com/problems/summary-ranges/
+ *
  * [56] Merge Intervals
  */
 
 // @lc code=start
-class Solution {
+class Solution
+{
 public:
-    vector<vector<int>> merge(vector<vector<int>>& intervals) {
-        
+    vector<vector<int>> merge(vector<vector<int>> &intervals)
+    {
+        int i = 0, len = intervals.size();
+        vector<vector<int>> mergedResult;
+
+        if (len == 0)
+            return mergedResult;
+
+        if (len == 1)
+            return intervals;
+
+        sort(intervals.begin(), intervals.end());
+        int start = intervals[0][0], end = intervals[0][1];
+
+        for (i = 1; i < len; i++)
+        {
+            if (end >= intervals[i][0]) //  IF 2nd ele of an arr >= 1st ele of the next arr  ===>  then Update END (means overlap).
+                end = max(intervals[i][1], end);
+            else
+            {
+                mergedResult.push_back({start, end});
+                start = intervals[i][0];
+                end = intervals[i][1];
+            }
+        }
+
+        mergedResult.push_back({start, end}); //  add the last pair
+
+        return mergedResult;
     }
 };
-// @lc code=end
 
+// @lc code=end
