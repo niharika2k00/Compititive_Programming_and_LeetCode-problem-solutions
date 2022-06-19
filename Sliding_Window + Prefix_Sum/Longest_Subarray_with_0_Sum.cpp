8742d1404527bc91a3f19d401d1d722b46bef04c
niharika2k00 <dniharika16@gmail.com>
2022-06-19 19:41:15 +0530

    DSA

---
+++ Sliding_Window + Prefix_Sum/Longest_Subarray_with_0_Sum.cpp
@@ -0,0 +1,63 @@
+
+/*
+________________________________________
+----------------------------------------
+ Author    :  Niharika Dutta
+ Code Link :
+ Time Complexity :  O(log n)    but unordered SET is O(1)
+________________________________________
+----------------------------------------
+ */
+#include <bits/stdc++.h>
+#include <iostream>
+
+using namespace std;
+
+#define ll long long
+#define vi vector<int>
+#define vll vector<ll>
+#define pb push_back
+#define mp make_pair
+#define loop1(n) for (ll i = 0; i < (n); i++)
+#define loop2(n) for (ll i = 1; i <= (n); i++)
+#define test  \
+    ll t;     \
+    cin >> t; \
+    while (t--)
+
+// https://www.codingninjas.com/codestudio/problems/920321?topList=striver-sde-sheet-problems&utm_source=striver&utm_medium=website&leftPanelTab=0
+
+int LongestSubsetWithZeroSum(vector<int> arr)
+{ // Prefix Sum Concept
+    unordered_map<int, int> hashMap;
+    int len = arr.size(), sum = 0, maxi = 0;
+
+    for (int i = 0; i < len; i++)
+    {
+        sum = sum + arr[i];
+
+        if (sum == 0)
+            maxi = i + 1; //  means the subarray len will be from the beginning
+
+        if (hashMap.find(sum) != hashMap.end()) // found
+            maxi = max(maxi, i - hashMap[sum]);
+        else
+            hashMap[sum] = i; // ( prefixSum , index)
+    }
+
+    return maxi;
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    vector<int> arr{1, 3, -1, 4, -4};
+
+    int result = LongestSubsetWithZeroSum(arr);
+    cout << "Length of the Longest Subarray with 0 sum is : " << result << endl;
+
+    return 0;
+}
