78427a6a7b9d07b42ba0cecefb150c2e49541604
niharika2k00 <dniharika16@gmail.com>
2022-06-24 23:55:57 +0530

    course schedule 3

---
+++ Leet_Code_Problems/630.course-schedule-iii.cpp
@@ -0,0 +1,18 @@
+/*
+ * @lc app=leetcode id=630 lang=cpp
+ *
+ * [630] Course Schedule III
+ *
+ * https://leetcode.com/problems/course-schedule-iii/
+ *
+ */
+
+// @lc code=start
+class Solution
+{
+public:
+    int scheduleCourse(vector<vector<int>> &courses)
+    {
+    }
+};
+// @lc code=end
