fb890b30a6ba88d58334347cd110819b0e994291
root <root@DESKTOP-VE68TKG.localdomain>
2022-07-05 18:09:07 +0530

    Binary tree rev

---
+++ Leet_Code_Problems/662.maximum-width-of-binary-tree.cpp
@@ -0,0 +1,29 @@
+/*
+ * @lc app=leetcode id=662 lang=cpp
+ *
+ * [662] Maximum Width of Binary Tree
+ *
+ * https://leetcode.com/problems/maximum-width-of-binary-tree/
+ *
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution
+{
+public:
+    int widthOfBinaryTree(TreeNode *root)
+    {
+    }
+};
+// @lc code=end
