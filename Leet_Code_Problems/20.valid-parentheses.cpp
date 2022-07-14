9859b4b13edb48c3ee289eff46f8581a742833d3
niharika2k00 <dniharika16@gmail.com>
2022-07-15 02:24:37 +0530

    Stack

---
+++ Leet_Code_Problems/20.valid-parentheses.cpp
@@ -0,0 +1,18 @@
+/*
+ * @lc app=leetcode id=20 lang=cpp
+ *
+ * [20] Valid Parentheses
+ *
+ * https://leetcode.com/problems/valid-parentheses/
+ *
+ */
+
+// @lc code=start
+class Solution
+{
+public:
+    bool isValid(string s)
+    {
+    }
+};
+// @lc code=end
