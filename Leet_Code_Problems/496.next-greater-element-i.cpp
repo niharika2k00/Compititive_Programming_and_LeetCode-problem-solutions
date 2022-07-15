306da955fee9e24c395712b42dd1f1447addc1d0
root <root@DESKTOP-VE68TKG.localdomain>
2022-07-15 13:18:11 +0530

    Stack Next Greater Element

---
+++ Leet_Code_Problems/496.next-greater-element-i.cpp
@@ -0,0 +1,18 @@
+/*
+ * @lc app=leetcode id=496 lang=cpp
+ *
+ * [496] Next Greater Element I
+ *
+ * https://leetcode.com/problems/next-greater-element-i/
+ *
+ */
+
+// @lc code=start
+class Solution
+{
+public:
+    vector<int> nextGreaterElement(vector<int> &nums1, vector<int> &nums2)
+    {
+    }
+};
+// @lc code=end
